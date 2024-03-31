be4a4ad32902f42841b2e28f1e50523b3cee5f9c
Beknazar Kemelov <zevs.312.777@gmail.com>
2024-03-31 21:29:30 +0600

    updating endpoints for AWS

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,9 +1,9 @@
 const API_PATHS = {
-  product: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  import: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  bff: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  product: "https://6ohm6rxov1.execute-api.eu-west-1.amazonaws.com/dev",
+  order: "https://6ohm6rxov1.execute-api.eu-west-1.amazonaws.com/dev",
+  import: "https://6ohm6rxov1.execute-api.eu-west-1.amazonaws.com/dev",
+  bff: "https://6ohm6rxov1.execute-api.eu-west-1.amazonaws.com/dev",
+  cart: "https://6ohm6rxov1.execute-api.eu-west-1.amazonaws.com/dev",
 };
 
 export default API_PATHS;
--- src/index.tsx
+++ src/index.tsx
@@ -14,23 +14,24 @@ const queryClient = new QueryClient({
   },
 });
 
-import("./mocks/browser").then(({ worker }) => {
+if (import.meta.env.DEV) {
+  const { worker } = await import("./mocks/browser");
   worker.start({ onUnhandledRequest: "bypass" });
+}
 
-  const container = document.getElementById("app");
-  // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-  const root = createRoot(container!);
-  root.render(
-    <React.StrictMode>
-      <BrowserRouter>
-        <QueryClientProvider client={queryClient}>
-          <ThemeProvider theme={theme}>
-            <CssBaseline />
-            <App />
-          </ThemeProvider>
-          <ReactQueryDevtools initialIsOpen={false} />
-        </QueryClientProvider>
-      </BrowserRouter>
-    </React.StrictMode>
-  );
-});
+const container = document.getElementById("app");
+// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+const root = createRoot(container!);
+root.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <QueryClientProvider client={queryClient}>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <App />
+        </ThemeProvider>
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
+    </BrowserRouter>
+  </React.StrictMode>
+);
